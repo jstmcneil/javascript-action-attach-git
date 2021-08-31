2e5eb502645847a40cc4c60a414c6deec39bd043
Jake McNeil <33610633+jstmcneil@users.noreply.github.com>
2021-08-31 12:15:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const template = core.getInput('template-name');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`Template ${nameToGreet}!`);
+  console.log(`Template ${template}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
